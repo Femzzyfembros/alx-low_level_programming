9484d432cff1ef0ddf812fe3e000babf7101f486
femzzyfembros <femzzyfembros@gmail.com>
2023-02-17 21:04:20 -0800

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
@@ -17,12 +18,15 @@ int main(void)
 			if (i < j && i != j)
 			{
 				putchar(i + '0');
-				putchar(j + ')');
-				putchar(',');
-				putchar(' ');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-	putchar('n');
+	putchar('\n');
 	return (0);
 }
